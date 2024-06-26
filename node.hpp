0576263963e95b5dafef0da94dac3eaa065fd4c4
TubiSmadar <tuviasmadar@gmail.com>
2024-06-26 21:45:13 +0300

    Upload files

---
+++ node.hpp
@@ -0,0 +1,15 @@
+#ifndef NODE_HPP
+#define NODE_HPP
+
+#include <vector>
+
+template <typename T>
+class Node {
+public:
+    T value;
+    std::vector<Node*> children;
+    Node(T val) : value(val) {}
+    T get_value() const { return value; }
+};
+
+#endif // NODE_HPP
